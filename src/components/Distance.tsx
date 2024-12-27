fc5ad40de2eace016d424b360e9c3353e3e534ff
Chris Franks <chris.franks@gymshark.com>
2024-12-27 20:42:26 +0000

    Test Edit Change

--- src/components/Distance.tsx
+++ src/components/Distance.tsx
@@ -1,14 +1,13 @@
 import { formatDistanceToNowStrict, format } from "date-fns";
 
 export const Distance = () => {
-  return "213 DAYS"
-  // const date = process.env.WEDDING_DATE;
-  // const formatted = formatDistanceToNowStrict(
-  //   format(date || "", "dd/MM/yyyy"),
-  //   {
-  //     unit: "day",
-  //   }
-  // );
+  const date = process.env.WEDDING_DATE;
+  const formatted = formatDistanceToNowStrict(
+    format(date || "", "dd/MM/yyyy"),
+    {
+      unit: "day",
+    }
+  );
 
-  // return formatted;
+  return formatted;
 };
